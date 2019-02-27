2d57c4837ec9be660ea6dd09cfc533e3c177a2f2
Uriel Robls <uriel_r@outlook.com>
2019-02-27 17:56:15 -0600

    licensias

---
+++ screens/views/Meat.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button, TextInput, Image, ScrollView } from 'react-native';
+import MenuBar from './MenuBar'
+
+export default class Meat extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+    render(){
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
